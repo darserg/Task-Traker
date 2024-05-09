65d964fec74b7ab8b3aa6d41a3ebb4658a8091fc
Darserg <Darserg1212>
2024-05-09 18:31:32 +0400

    Initial Commit

---
+++ Task-Traker/Task_TrakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_TrakerApp.swift
+//  Task-Traker
+//
+//  Created by Сергей Дарьин on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Task_TrakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
